c566d7db67a00bc4b52676994da82a6824dd6ef8
Akash Mishra <07akashmishra07@gmail.com>
2022-12-12 21:05:42 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const exphbs  = require('express-handlebars');
+const path =require('path');
+
+const PORT = process.env.PORT || 5000;
+
+//Set Handlebars Milddleware
+app.engine('handlebars', exphbs.engine({layoutsDir: __dirname + '/views/layouts',}));
+app.set('view engine', 'handlebars');
+
+const otherstuff="Hello there,this is raze stuff";
+
+
+//set handlebar routes
+app.get('/', function (req, res) {
+    res.render('home',{
+        stuff:otherstuff,
+    });
+});
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => console.log('Server listening on port' + PORT));
